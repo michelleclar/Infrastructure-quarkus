c33c2741c3eb3a6bc4432b6287f7b9ad52eb4ce8
carl <carlmichelle493@gmail.com>
2025-01-03 16:11:52 +0800

    init add maven push

---
+++ persistence/src/main/java/org/carl/engine/runtime/JooqRecorder.java
@@ -0,0 +1,14 @@
+package org.carl.engine.runtime;
+
+import io.quarkus.runtime.annotations.Recorder;
+import org.jboss.logging.Logger;
+
+/**
+ * Quarkus Template class (runtime)
+ *
+ * @author <a href="mailto:leo.tu.taipei@gmail.com">Leo Tu</a>
+ */
+@Recorder
+public class JooqRecorder {
+    private static final Logger log = Logger.getLogger(JooqRecorder.class);
+}
